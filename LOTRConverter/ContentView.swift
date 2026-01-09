c9d5b1ca6c8fe7def9e27f2a35b006f7746e6fbc
Ritika <bhardwajritika11@gmail.com>
2026-01-10 02:47:31 +0530

    Initial Commit

---
+++ LOTRConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Tarun Sharma on 10/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Tarun Sharma on 10/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
